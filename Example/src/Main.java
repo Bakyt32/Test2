6309e7858bbfbab04613b09a34de9fac42de0064
User <user@gmail.com>
2023-11-19 19:22:44 +0100

    add function

--- Example/src/Main.java
+++ Example/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int a = 900000;
+        int b = 1000;
+        System.out.println(add(a, b));
+    }
+
+    private static int add(int a, int b){
+        return a + b;
     }
 }
