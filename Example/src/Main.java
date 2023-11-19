941c7933899b7529669c79215a9a7f76d05b4a9b
User <user@gmail.com>
2023-11-19 19:24:33 +0100

    add sub

--- Example/src/Main.java
+++ Example/src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 900000;
-        int b = 1000;
+        int a = -1;
+        int b = 20;
         System.out.println(add(a, b));
+        System.out.println(sub(a, b));
     }
 
     private static int add(int a, int b){
         return a + b;
+    }
+    private static int sub(int a, int b){
+        return a - b;
     }
 }
